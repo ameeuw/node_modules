0171fd4790363bab67299a829ce723ddf8a6e540
ameeuw <arne.meeuw@gmail.com>
2016-07-11 23:20:23 +0200

    Reduced RGBled

--- RGBled.lua
+++ RGBled.lua
@@ -31,7 +31,6 @@ function RGBled.new(mode, pins, timer)
 		-- RGB LED pins:
 		self.pins = {['r']=pins[1], ['g']=pins[2], ['b']=pins[3]}
 		self.mode = "PWM"
-
 		-- Set PWM modes
 		for k,pin in pairs(self.pins) do
 			pwm.setup(pin,300,0)
@@ -47,7 +46,7 @@ function RGBled.new(mode, pins, timer)
 		else
 			self.pins = {['ws']=pins}
 		end
-			self.mode = "WS2812"
+		self.mode = "WS2812"
 	end
 
 	if timer~=nil then
@@ -60,17 +59,12 @@ function RGBled.new(mode, pins, timer)
 end
 
 function RGBled.blink(self, times, delay, r, g, b)
-	local lighton=0
+	local on=false
 	local count=0
 	tmr.alarm(self.timer,delay,1,
 		function()
-			if lighton==0 then
-				lighton=1
-				self.setRgb(self, r, g, b)
-			else
-				lighton=0
-				self.setRgb(self, 0, 0, 0)
-			end
+			on = not on
+			self:setRgb( (on and 1 or 0)*r, (on and 1 or 0)*g, (on and 1 or 0)*b )
 			if count==(times*2-1) then
 				tmr.stop(self.timer)
 			else
@@ -93,9 +87,7 @@ function RGBled.breathe(self, times, r, g, b)
             local tB = b * dim / maxSteps
             self:setRgb(tR, tG, tB)
             dim = dim + direction
-            if dim > maxSteps then
-                direction = -1
-            end
+            if dim > maxSteps then direction = -1 end
             if dim < minSteps then
                 direction = 1
                 count = count + 1
@@ -111,81 +103,52 @@ function RGBled.fade(self, r, g, b)
 	local step = 0
 	local steps = 35
 	local stepDelay = 20
-	local dr = (r - self.oldColor.r)
-	local dg = (g - self.oldColor.g)
-	local db = (b - self.oldColor.b)
-
 	tmr.alarm(self.timer,stepDelay,tmr.ALARM_AUTO,
 		function()
 			step = step + 1
-			local cr = math.max(0, self.oldColor.r + step * dr / steps)
-			local cg = math.max(0, self.oldColor.g + step * dg / steps)
-			local cb = math.max(0, self.oldColor.b + step * db / steps)
-
+			-- linear function 'start + x * dc / c'
+			local cr = math.max(0, self.oldColor.r + step * (r - self.oldColor.r) / steps)
+			local cg = math.max(0, self.oldColor.g + step * (g - self.oldColor.g) / steps)
+			local cb = math.max(0, self.oldColor.b + step * (b - self.oldColor.b) / steps)
 			self.setRgb(self, cr, cg, cb)
-
 			if step > steps then
 				tmr.stop(self.timer)
-				self.oldColor.r = r
-				self.oldColor.g = g
-				self.oldColor.b = b
-				self.setRgb(self, r, g, b)
+				self.oldColor.r, self.oldColor.g, self.oldColor.b = r, g, b
+				self:setRgb(r, g, b)
 			end
 		end)
 end
 
 function RGBled.setRgb(self, r, g, b)
-
 	if self.mode == "PWM" then
 		pwm.setduty(self.pins.r, r)
 		pwm.setduty(self.pins.g, g)
 		pwm.setduty(self.pins.b, b)
 	end
-
 	if self.mode == "WS2812" then
 		ws2812.writergb(self.pins.ws, string.char(r,g,b))
 	end
-
-	self.color.r = r
-	self.color.g = g
-	self.color.b = b
+	self.color.r, self.color.g, self.color.b = r, g, b
 end
 
-function RGBled.setHsb(self, h, s, b)
-	--[[ INTEGER VERSION!
-	 * Converts an HSV color value to RGB. Conversion formula
-	 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-	 * Assumes h e[0,360], s e[0, 100] and v e[0,100]
-	 * returns r, g, and b in the set [0, 255].
-	 *
-	 * @param   Number  h       The hue
-	 * @param   Number  s       The saturation
-	 * @param   Number  v       The value
-	 * @return  Array           The RGB representation
-	]]
-	local function hsvToRgb(h, s, v)
-	  local r, g, b
-	  h = h * 5 / 18
-	  local i = math.floor(h * 6) - (math.floor(h * 6) % 100);
-	  local f = h * 6 - i;
-	  local p = (v * (100 - s)) / 100;
-	  local q = v * (10000 - f * s) / 10000;
-	  local t = v * (10000 - (100 - f) * s) / 10000;
-	  i = i % 600
+function RGBled.setHsb(self, h, s, v)
+  local r, g, b
+  h = h * 5 / 18
+  local i = math.floor(h * 6) - (math.floor(h * 6) % 100);
+  local f = h * 6 - i;
+  local p = (v * (100 - s)) / 100;
+  local q = v * (10000 - f * s) / 10000;
+  local t = v * (10000 - (100 - f) * s) / 10000;
+  i = i % 600
 
-	  if i == 0 then r, g, b = v, t, p
-	  elseif i == 100 then r, g, b = q, v, p
-	  elseif i == 200 then r, g, b = p, v, t
-	  elseif i == 300 then r, g, b = p, q, v
-	  elseif i == 400 then r, g, b = t, p, v
-	  elseif i == 500 then r, g, b = v, p, q
-	  end
-
-	  return r * 255 / 100, g * 255 / 100, b * 255 / 100
-	end
-
-	self:fade(hsvToRgb(h,s,b))
-
+  if i == 0 then r, g, b = v, t, p
+  elseif i == 100 then r, g, b = q, v, p
+  elseif i == 200 then r, g, b = p, v, t
+  elseif i == 300 then r, g, b = p, q, v
+  elseif i == 400 then r, g, b = t, p, v
+  elseif i == 500 then r, g, b = v, p, q
+  end
+	self:fade(r * 255 / 100, g * 255 / 100, b * 255 / 100)
 end
 
 function RGBled.stop(self)
