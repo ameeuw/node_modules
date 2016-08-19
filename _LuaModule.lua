70e778b5eda0c30244abccfa3277c34c6fb4aaa5
ameeuw <arne.meeuw@gmail.com>
2016-08-19 15:31:06 +0200

    Updated LuaModule template
    Updated with syntax updates (function naming and name adaption)

--- _LuaModule.lua
+++ _LuaModule.lua
@@ -1,14 +1,16 @@
--- LuaModule.lua module
--- Author: Arne Meeuw
--- github.com/ameeuw
---
--- Skeleton for creating a LuaModule
---
--- Initialize:
--- LuaModule = require('LuaModule').new()
---
--- Methods:
--- LuaModule:function()
+--[[
+LuaModule.lua module
+Author: Arne Meeuw
+github.com/ameeuw
+
+Skeleton for creating a LuaModule
+
+Initialize:
+LuaModule = require('LuaModule').new()
+
+Methods:
+LuaModule:function()
+ --]]
 
 local LuaModule = {}
 LuaModule.__index = LuaModule
@@ -16,15 +18,12 @@ LuaModule.__index = LuaModule
 function LuaModule.new()
 
 	local self = setmetatable({}, LuaModule)
-
-	if name == nil then
-		name = 'LuaModule:'..string.sub(wifi.sta.getmac(),13,-1)
-	end
+	name = name or 'LuaModule:'..string.sub(wifi.sta.getmac(),13,-1)
 
 	return self
 end
 
-function LuaModule.function(self)
+function LuaModule:function()
   print("function call!")
 end
 
