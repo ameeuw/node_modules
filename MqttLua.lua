2809ec537135e508db0366c88d8e76a43f0f7ba5
ameeuw <arne.meeuw@gmail.com>
2016-08-19 15:34:50 +0200

    Updated MqttLua
    Allows for remote terminal included in ./IDE directory.

--- MqttLua.lua
+++ MqttLua.lua
@@ -4,17 +4,19 @@
 --
 
 -- Declare global name concatenated from purpose-name and MAC address
-name = 'MqttClient:'..string.sub(wifi.sta.getmac(),13,-1)
+name = name or 'MqttClient:'..string.sub(wifi.sta.getmac(),13,-1)
 
+
+function beginMQTT()
 -- Initialize Esp module and add begin() callback
-Esp = require("Esp").new(
+Esp = Esp or require("Esp").new(
     function()
         MqttClient = require("MqttClient").new('m-e-e-u-w.de', 62763)
         MqttClient:register("telnet/input",
             function(topic, message)
                 node.input(message)
             end)
-        
+
         function s_output(str)
         -- Filter non printable characters
             local string,_ = string.gsub(str,'%c','')
@@ -22,7 +24,36 @@ Esp = require("Esp").new(
                 MqttClient.MqttClient:publish(MqttClient.topic.."telnet/output", string,0,0)
             end
         end
-        
+
         net.dns.setdnsserver('8.8.8.8',0)
         node.output(s_output,1)
     end)
+end
+
+function beginWS()
+  Esp = Esp or require("Esp").new(
+    function()
+      -- mdns.register("fishtank", {hardware='NodeMCU'})
+      mdns.register(name, {hardware='ESP866'})
+      server = net.createServer(net.TCP, 30)
+      server:listen(62763,
+        function(connection)
+
+          connection:on("receive",
+            function(c, string)
+              node.input(string)
+            end)
+
+        function s_output(str)
+        -- Filter non printable characters
+          local string,_ = string.gsub(str,'%c','')
+          if (string~="") then
+            connection:send(string)
+          end
+        end
+        node.output(s_output,1)
+      end
+      )
+    end
+  )
+end
